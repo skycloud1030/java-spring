f705e1f0f1901825602f56461f424bc2c8146b3e
CT Hung <skycloud1030@gmail.com>
2020-06-22 00:29:49 +0800

    create spring boot MVC - Add restful router with PostgreSQL - Add jpa

---
+++ README.md
@@ -0,0 +1,21 @@
+### Generate spring folder
+
+- https://start.spring.io/
+
+
+### gradle
+- org.springframework.boot:spring-boot-starter-web
+- org.springframework.boot:spring-boot-starter-data-jpa
+- org.postgresql:postgresql
+
+
+### Run the Application
+```sh
+./gradlew bootRun
+```
+
+
+### Mount folder in virtual box
+```sh
+mount -t vboxsf folder /folder
+```
---
+++ src/demo/src/main/model/User.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  @NotNull
+  private String email;
+
+  @NotNull
+  private String name;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
